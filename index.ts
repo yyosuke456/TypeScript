2b1bd12b8d950dd645205c8fbde51aabef66bbd0
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-05-25 23:39:44 +0900

    変数に型付け

--- index.ts
+++ index.ts
@@ -5,3 +5,12 @@ console.log(hello);
 function add(a: number, b: number) : number {
   return a + b;
 }
+
+let hasValue: boolean = true;
+let count: number = 10;
+let float:number = 3.14;
+let negative: number = - 0.12;
+let single: string  = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+
