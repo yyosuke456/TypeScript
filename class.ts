40f7d1abd868a5536247f611aa7dfd659b672537
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-06-16 00:08:52 +0900

    クラスメソッド

--- class.ts
+++ class.ts
@@ -3,7 +3,15 @@ class Person {
   constructor(iniitName: string){
     this.name = iniitName;
   }
+
+  //クラスメソッド
+  greeting() {
+    console.log(`hello, my name is ${this.name}`);
+  }
 }
 
 const quill = new Person('Quill');
 console.log(quill);
+
+const anontherQuill = new Person('Quill');
+quill.greeting();
