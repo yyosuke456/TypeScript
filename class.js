50da12a8de9c960fd1245ec76ac903503f821d37
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-06-15 23:31:17 +0900

    compile

---
+++ class.js
@@ -0,0 +1,8 @@
+var Person = /** @class */ (function () {
+    function Person(iniitName) {
+        this.name = iniitName;
+    }
+    return Person;
+}());
+var quill = new Person('Quill');
+console.log(quill);
