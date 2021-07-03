27f3afbcafb4c068ede3bc07b7aaa3aa7014a419
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-07-03 20:05:42 +0900

    strictNullCheckで型定義を厳しくする

--- tmp/compiler.ts
+++ tmp/compiler.ts
@@ -1,2 +1,7 @@
 let beUpperHello = 'hello';
 console.log(beUpperHello.toUpperCase());
+
+let undefinedableMessage: string =  undefined;
+let nullableMessage: string = null;
+let onlyNull: null = undefined;
+let onlyUndefined: undefined = null;
