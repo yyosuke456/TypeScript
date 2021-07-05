e8b2ba7084e489bd4da2f99d274862e122726714
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-07-05 22:10:22 +0900

    使われていないローカル変数は禁止

--- tmp/compiler.ts
+++ tmp/compiler.ts
@@ -1,7 +1,7 @@
 let beUpperHello = 'hello';
 console.log(beUpperHello.toUpperCase());
 
-let undefinedableMessage: string =  undefined;
-let nullableMessage: string = null;
-let onlyNull: null = undefined;
-let onlyUndefined: undefined = null;
+function test(){
+  let hello = 'hello';
+  console.log(hello);
+}
