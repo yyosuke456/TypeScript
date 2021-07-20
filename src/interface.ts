62a8041321ce26e4be73e7a9c8603e5272bb81aa
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-07-20 22:30:10 +0900

    構造的部分型その２

--- src/interface.ts
+++ src/interface.ts
@@ -14,3 +14,12 @@ class Developer implements Human {
 const user: Human = new Developer('Quill', 38, 8);// Human型で定義できる。
 user.age;// Human型なのでuser.experienceは呼べない。構造的部分型
 
+const tmpDeveloper = {
+  name: 'Quill',
+  age: 38,
+  experience: 8,
+  greeting(message: string){
+    console.log(message)
+  }
+}
+const tmpUser: Human = tmpDeveloper;// これもエラーにならない。
