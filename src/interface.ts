3862cd3708a23266dbe1a23ad2851fc5003904a7
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-07-24 20:09:58 +0900

    interfaceで関数型を表現する

--- src/interface.ts
+++ src/interface.ts
@@ -1,3 +1,12 @@
+//type addFunc = (num1: number, num2: number) => number;
+interface addFunc{
+  (num1: number, num2: number): number;
+}
+let addFunc: addFunc;
+addFunc = (n1: number, n2: number) => {
+  return n1 + n2;
+}
+
 interface Nameable{
   name: string;
 }
