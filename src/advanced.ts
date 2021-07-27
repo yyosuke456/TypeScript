4ac8786c51086b4b02e9c0748b816e77e2b512f9
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-07-27 23:35:34 +0900

    &で複数typeのインターセクション型を定義

---
+++ src/advanced.ts
@@ -0,0 +1,17 @@
+type Engineer = {
+  name: string;
+  role: string;
+}
+
+type Blogger = {
+  name: string;
+  follower: number;
+}
+
+type EngineerBlogger = Engineer & Blogger;
+
+const michel : EngineerBlogger = {
+  name: 'michel',
+  role: 'back-end',
+  follower: 100
+}
